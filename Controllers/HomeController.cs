1446c402d3c316ff8f922f08433398867906ef34
imbps <imbps@LAPTOP-LB60RIFK>
2022-03-15 00:48:39 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using RaviCURDWebApp.DB_Connect_RP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RaviCURDWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            prakashEntities obj = new prakashEntities();
+            
+            var res=obj.stud_info.ToList();
+
+            return View(res);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/PrakashDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaviCURDWebApp.Models
+{
+    public class PrakashDetails
+    {
+        public int Std_id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string email { get; set; }
+        public string country { get; set; }
+    }
+}
